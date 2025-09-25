731f3a4670478e423527c20a9a2e44b8c181f872
TijmenSimons <61019437+troshujin@users.noreply.github.com>
2025-09-25 10:00:00 +0200

    api url fix

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_BASE_URL || '/api/latest';
+const API_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const CLOUDINARY_CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME
 export const CLOUDINARY_API_KEY = import.meta.env.VITE_CLOUDINARY_API_KEY
@@ -7,9 +7,10 @@ export const CLOUDINARY_API_ENV_VAR = import.meta.env.VITE_CLOUDINARY_API_ENV_VA
 
 function getApiBaseUrl() {
     // If running in Docker/production, use relative URL
-    // if (window.location.hostname !== 'localhost' || !window.location.port.startsWith('51')) {
-    //     return '/api/latest';
-    // }
+    if (window.location.hostname !== 'localhost' || !window.location.port.startsWith('51')) {
+        console.log(`https://api.${window.location.hostname}/`)
+        return `https://api.${window.location.hostname}/`;
+    }
     // If in development mode, use the full URL
     return API_URL;
 }
